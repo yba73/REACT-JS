145ed654588af664e20451ba972aad3de4e394cc
origin <yba73mos@gmail.com>
2022-05-13 10:18:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form' ;
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+<>
+  <Form.Group className="mb-3">
+    <Form.Label>Disabled input</Form.Label>
+    <Form.Control placeholder="Disabled input" disabled />
+  </Form.Group>
+  <Form.Group className="mb-3">
+    <Form.Label>Disabled select menu</Form.Label>
+    <Form.Select disabled>
+      <option>Disabled select</option>
+    </Form.Select>
+  </Form.Group>
+  <Form.Group className="mb-3">
+    <Form.Check type="checkbox" label="Can't check this" disabled />
+  </Form.Group>
+</>
     </div>
   );
 }
